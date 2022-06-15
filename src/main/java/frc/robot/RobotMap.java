087381052da924154e6d8b1be9a2f0d0df83e8d0
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2022-06-15 13:35:09 -0700

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -11,23 +11,23 @@ public final class RobotMap {
     public static final class map_Drivetrain {
 
         // CAN IDS 
-        public static final int FRONT_LEFT_DRIVE = 0; // left front drive
         public static final int FRONT_LEFT_TURN = 1;  // left front steer
+        public static final int FRONT_LEFT_DRIVE = 2; // left front drive
 
-        public static final int FRONT_RIGHT_DRIVE = 2; // right front drive
-        public static final int FRONT_RIGHT_TURN = 3; // right front steer
+        public static final int BACK_LEFT_TURN = 3; // left rear steer
+        public static final int BACK_LEFT_DRIVE = 4; // left rear drive
 
-        public static final int BACK_LEFT_DRIVE = 4; // left back drive
-        public static final int BACK_LEFT_TURN = 5; // left back steer
+        public static final int BACK_RIGHT_TURN = 5; // right rear steer
+        public static final int BACK_RIGHT_DRIVE = 6; // right rear drive
 
-        public static final int BACK_RIGHT_DRIVE = 6; // right back drive
-        public static final int BACK_RIGHT_TURN = 7; // right back steer
+        public static final int FRONT_RIGHT_TURN = 7; // right front steer
+        public static final int FRONT_RIGHT_DRIVE = 8; // right front drive
 
         // ANALOG INPUT PORTS
         public static final int FRONT_LEFT_TURN_ENCODER = 0; // left front
-        public static final int FRONT_RIGHT_TURN_ENCODER = 1; // right front
-        public static final int BACK_LEFT_TURN_ENCODER = 2; // left back
-        public static final int BACK_RIGHT_TURN_ENCODER = 3; // right back
+        public static final int BACK_LEFT_TURN_ENCODER = 1; // left rear
+        public static final int BACK_RIGHT_TURN_ENCODER = 2; // right rear
+        public static final int FRONT_RIGHT_TURN_ENCODER = 3; // right front
     }
 
 }
